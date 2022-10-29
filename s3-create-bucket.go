fb486dc1eb33f72a66c494ebd831c80d9e4e2a94
jayanthkrishna <gannerla16038@iiitd.ac.in>
2022-10-29 23:17:42 +0530

    Added upload and delete functions

--- s3-create-bucket.go
+++ s3-create-bucket.go
@@ -7,12 +7,18 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	// "github.com/aws/aws-sdk-go/aws/credentials"
 )
 
 func exitErrorf(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
 	os.Exit(1)
+}
+
+type service struct {
+	svc  *s3.S3
+	sess *session.Session
 }
 
 func main() {
@@ -29,9 +35,23 @@ func main() {
 	})
 
 	// Create S3 service client
-	svc := s3.New(sess)
+	var s service
 
-	_, err := svc.CreateBucket(&s3.CreateBucketInput{
+	s.svc = s3.New(sess)
+	s.sess = sess
+
+	filename := os.Args[2]
+
+	// s.CreateBucket(bucket)
+	item := s.UploadFiletoBucket(bucket, filename)
+	s.DownloadFileFromBucket(bucket, item)
+
+}
+
+func (s *service) CreateBucket(bucket string) {
+
+	// s3.ErrCodeBucketAlreadyExists
+	_, err := s.svc.CreateBucket(&s3.CreateBucketInput{
 		Bucket: aws.String(bucket),
 	})
 	if err != nil {
@@ -41,7 +61,7 @@ func main() {
 	// Wait until bucket is created before finishing
 	fmt.Printf("Waiting for bucket %q to be created...\n", bucket)
 
-	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
+	err = s.svc.WaitUntilBucketExists(&s3.HeadBucketInput{
 		Bucket: aws.String(bucket),
 	})
 
@@ -50,5 +70,68 @@ func main() {
 	}
 
 	fmt.Printf("Bucket %q successfully created\n", bucket)
+}
+
+func (s *service) ListBucket(bucket string) {
+
+	resp, err := s.svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket)})
+	if err != nil {
+		exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
+	}
+
+	for _, item := range resp.Contents {
+		fmt.Println("Name:         ", *item.Key)
+		fmt.Println("Last modified:", *item.LastModified)
+		fmt.Println("Size:         ", *item.Size)
+		fmt.Println("Storage class:", *item.StorageClass)
+		fmt.Println("")
+	}
+
+}
+
+func (s *service) UploadFiletoBucket(bucket, filename string) string {
+
+	file, err := os.Open(filename)
+	if err != nil {
+		exitErrorf("Unable to open file %q, %v", err)
+	}
+
+	defer file.Close()
+
+	uploader := s3manager.NewUploader(s.sess)
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filename),
+		Body:   file,
+	})
+	if err != nil {
+		// Print the error and exit.
+		exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
+	}
+
+	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+
+	return filename
+
+}
+
+func (s *service) DownloadFileFromBucket(bucket, item string) *os.File {
+
+	downloader := s3manager.NewDownloader(s.sess)
+
+	var file *os.File
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(item),
+		})
+	if err != nil {
+		exitErrorf("Unable to download item %q, %v", item, err)
+	}
+
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+
+	return file
 
 }
